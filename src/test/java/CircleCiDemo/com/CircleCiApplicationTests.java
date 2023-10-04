6f952eb846b3b5e803ef78c4f0365240ab29f797
tonythomasinfo <94329816+tonythomasinfo@users.noreply.github.com>
2023-10-04 22:36:19 +1100

    First commit

---
+++ src/test/java/CircleCiDemo/com/CircleCiApplicationTests.java
@@ -0,0 +1,13 @@
+package CircleCiDemo.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircleCiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
